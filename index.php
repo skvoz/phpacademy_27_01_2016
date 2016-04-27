ed0acb3cec7e4d778b949804d4e83034aa129f04
Perevozchykov Kostiantyn <skvoz.ne@gmail.com>
2016-04-27 20:53:29 +0300

    commit

--- index.php
+++ index.php
@@ -15,14 +15,24 @@ $driver = 'mysql:host=localhost;dbname=scotchbox;';
 
 $db = new PDO($driver, $user, $password);
 
-$sql = 'select * from user limit 10';
+$sql = 'select * from user ORDER BY ID DESC limit 10 ';
 
 $result = $db->query($sql);
 
 $data = $result->fetchAll();
 
-include __DIR__ . "/src/views/index.php";
+if (isset($_REQUEST['save'])) {
+    $name = $_REQUEST['user_name'];
+    $description = $_REQUEST['description'];
+    $sql2 = sprintf('insert into `user` (user_name, description) VALUES ("%s","%s")',
+        $name, $description );
 
+    $db->query($sql2);
+    header('location: /index.php');
+}
+
+
+include __DIR__ . "/src/views/index.php";
 
 ?>
 
--- src/views/index.php
+++ src/views/index.php
@@ -1,6 +1,7 @@
 
 <form method="post" action="index.php">
     <input type="text" name="user_name" value=""/>
+    <input type="text" name="description" value=""/>
     <input type="submit" name="save" value="save"/>
 </form>
 <table border="1" cellpadding="1" cellspacing="1">
